92f016599708fa88471807783bc821b212d34d31
mz2458594 <mz2458594@gmail.com>
2025-07-31 18:57:24 -0500

    Implementando función para agregar

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-let listaAmigos = {};
+let listaAmigos = [];
 
 
 function agregarAmigo(){
-    let input = document.getElementById('amigo').value;
-    if (input === '') {
-        alert('Ingrese un nombre');
+    let input = document.getElementById('amigo');
+    if (input.value === '' || listaAmigos.includes(input.value)) {
+        alert('Por favor, ingrese un nombre válido');
         return;
+    } else {
+        listaAmigos.push(input.value);
+        console.log(listaAmigos);
+        input.value = '';
     }
 }
 
