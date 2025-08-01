5370cbfbdf3941030501388d6043b1f37da4336d
mz2458594 <mz2458594@gmail.com>
2025-07-31 19:20:55 -0500

    Implementando una funcion para mostrar los usuarios

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 let listaAmigos = [];
 
-
-function agregarAmigo(){
+function agregarAmigo() {
     let input = document.getElementById('amigo');
     if (input.value === '' || listaAmigos.includes(input.value)) {
         alert('Por favor, ingrese un nombre válido');
         return;
     } else {
         listaAmigos.push(input.value);
-        console.log(listaAmigos);
         input.value = '';
+        agregarLista();
     }
 }
 
-function sortearAmigo(){
+function sortearAmigo() {
+    if (listaAmigos != null){
+        alert('Ingrese usuarios');
+    } else {
+        
+    }
+}
 
+function agregarLista() {
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = '';
+    for (let index = 0; index < listaAmigos.length; index++) {
+        let li = document.createElement('li');
+        let element = listaAmigos[index];
+        li.appendChild(document.createTextNode(`${element}`))
+        lista.appendChild(li);
+    }
 }
